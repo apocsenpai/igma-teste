1b64c7ab2ba7c7aa72ec97ae6e57de1d8c652fdb
apocsenpai <jonatassennas@ufrrj.br>
2024-02-01 15:52:53 -0300

    feat: implements header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import Logo from "@/assets/LogoHeader.svg"
+import User from "@/assets/icon-user.svg"
+
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div>
+      <header className="w-full h-stack-xxl pr-inline-xxs flex items-center justify-between ">
+        <img src={Logo} alt="logotipo" className="w-inline-xl" />
+        <button className="w-inline-sm h-stack-sm bg-primary-main rounded-circle flex justify-center items-center">
+          <img src={User} alt="user button" />
+        </button>
+      </header>
+      <main>
+        
+      </main>
+    </div>
+  );
+};
 
-export default App
+export default App;
